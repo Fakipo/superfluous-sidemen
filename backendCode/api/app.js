0961fd8d0ab55b87614b621d05c32c12f90c035b
=Paras <=>
2022-03-01 21:45:51 +0530

    Create axios request to fetch the data in console

--- backendCode/api/app.js
+++ backendCode/api/app.js
@@ -6,22 +6,23 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+testAPIRouter = require("./routes/testAPI")
 var app = express();
-
+var cors = require("cors");
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use("/testApi",testAPIRouter)
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- frontendCode/client/src/App.js
+++ frontendCode/client/src/App.js
@@ -1,25 +1,99 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import home from './HomePage'
+import {tableData, teamNumber} from './DefaultValues';
+import JsonData from './PremierLeagueData.json';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import PlData from './PLDataFetch';
+import PersonList from './Persons.js';
+import Axios from 'axios';
+// const abc = JsonData.map((info)=>{
+//   return(
+//     <tr>
+//     <td>{info.id}</td>
+//     <td>{info.points}</td>
+//     <td>{info.teamName}</td>
+//     </tr>
+//   );
+// })
 
-function App() {
+console.log(PlData);
+console.log("three fpor");
+
+
+
+var axios = require("axios").default;
+
+var options = {
+  method: 'GET',
+  url: 'https://api-football-beta.p.rapidapi.com/standings',
+  params: {season: '2021', league: '39'},
+  headers: {
+    'x-rapidapi-host': 'api-football-beta.p.rapidapi.com',
+    'x-rapidapi-key': 'c587e26207mshe5ee3942027761ep140d32jsn34a6ec8354eb'
+  }
+};
+
+const abc = () => axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
+
+
+
+const displayData = JsonData.map((info) => {
+  return(
+    <tr>
+      <td>{info.id}</td>
+      <td>{info.teamName}</td>
+      <td>{info.points}</td>
+    </tr>
+  )
+}
+)
+
+
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {apiResponse : ""};
+  }
+
+  callAPI(){
+    fetch("http://localhost:9000/testAPI")
+      .then(res => res.text())
+      .then(res => this.setState({apiResponse: res}));
+  }
+
+  componentWillMount(){
+    this.callAPI();
+  }
+render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Hello this is the home page</h2>
+      <p>{this.state.apiResponse}</p>
+      {home()}
+      <button onClick={abc}> Click on this button for data in db</button>
+      <table className = "table">
+        <thead>
+        <tr>
+        <th> S. no</th>
+        <th>Team Name</th>
+        <th>Points </th>
+        </tr>
+        </thead>
+        <tbody>
+          {displayData}
+        </tbody>
+      </table>
+      <PersonList/>
     </div>
+    
   );
 }
-
+}
 export default App;
